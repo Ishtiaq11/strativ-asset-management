30204a6867bca159e48da0c43cf6a25a4e2e4f78
Ishtiaq Pias <ishtiaqpias@gmail.com>
2022-01-12 16:36:47 +0600

    Add asset management module

---
+++ assets/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Asset Management',
+    'version': '1.0',
+    'category': 'Tools',
+    'summary': 'Strativ Asset Management System',
+    'depends': ['web'],
+    'data': [
+    ],
+    'demo': [
+    ],
+    'css': [],
+    'installable': True,
+    'auto_install': False,
+    'application': True,
+}
